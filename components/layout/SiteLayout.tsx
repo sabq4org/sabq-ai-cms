1e6ea7fec3e2fb1ddd32c4b8485ee80e0b6f9033
sabq4org <44604145+sabq4org@users.noreply.github.com>
2025-09-28 02:46:09 -0400

    إعادة استخدام StaticHeader بدلاً من SmartHeader لإعادة الهيدر والعضوية كما كانت

--- components/layout/SiteLayout.tsx
+++ components/layout/SiteLayout.tsx
@@ -2,7 +2,7 @@
 
 import Footer from "@/components/Footer";
 import FooterGate from "@/components/layout/FooterGate";
-import SmartHeader from "@/components/layout/SmartHeader";
+import { StaticHeader } from "@/components/layout/StaticHeader";
 import LightHeader from "@/components/layout/LightHeader";
 import DesktopCategoryBar from "@/components/navigation/DesktopCategoryBar";
 import { Providers } from "../../app/providers";
@@ -84,7 +84,7 @@ export default function SiteLayout({
             {useLightHeader ? (
               <LightHeader />
             ) : (
-              <SmartHeader user={user} />
+              <StaticHeader />
             )}
             {/* المحتوى الرئيسي للموبايل مع الإزاحة الصحيحة */}
             <main style={{
@@ -102,7 +102,7 @@ export default function SiteLayout({
               {useLightHeader ? (
                 <LightHeader />
               ) : (
-                <SmartHeader user={user} />
+                <StaticHeader />
               )}
             </div>
             {/* شريط التصنيفات أسفل الهيدر - ديسكتوب فقط */}
