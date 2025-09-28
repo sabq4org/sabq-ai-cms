725b510fa2e5692bdebe3b5b4f95f724ea97557b
Ali Alhazmi <ali@sabq.ai>
2025-09-28 23:31:04 +0300

    Revert "fix(build): remove React Query DevTools import to fix production build"
    This reverts commit e3e1baa7d4b923b94a50c98a51090976ac50afdb.

--- providers/QueryProvider.tsx
+++ providers/QueryProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
 export default function QueryProvider({
@@ -35,6 +36,9 @@ export default function QueryProvider({
   return (
     <QueryClientProvider client={queryClient}>
       {children}
+      {process.env.NODE_ENV === "development" && (
+        <ReactQueryDevtools initialIsOpen={false} position="bottom" />
+      )}
     </QueryClientProvider>
   );
 }
